38ba3563a2003bf600c4985aed214d2fd63b8b9e
ZartDrag <kartikmay25@gmail.com>
2023-01-08 15:19:52 +0530

    Connection to database established

---
+++ db.js
@@ -0,0 +1,13 @@
+//will be used to connect to the database
+const mongoose = require('mongoose'); //import statement
+const mongoURI = "mongodb+srv://katak:kitrak@cluster0.lgnhsk2.mongodb.net/?retryWrites=true&w=majority"
+//MongoDB connection string
+
+const connectToMongo = ()=>{
+    mongoose.connect(mongoURI, ()=>{
+        //this function only fires when the connection is established; async in nature
+        console.log("Connected to Mongo Successfully");
+    })
+}
+
+module.exports = connectToMongo; //this makes connectToMongo() publically accessible
