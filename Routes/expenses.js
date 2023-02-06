76ae8317ad4262beee1628ecfc480bc1a7078842
ZartDrag <kartikmay25@gmail.com>
2023-02-06 18:14:26 +0530

    Deleteion API added

--- Routes/expenses.js
+++ Routes/expenses.js
@@ -5,7 +5,7 @@ const Expense = require("../models/Expense");
 const { body, validationResult } = require("express-validator");
 const { response } = require("express");
 
-//Get all the expenses of a user; Login Required
+//Route 1: Get all the expenses of a user; Login Required
 router.get("/getallexpenses", fetchuser, async (req, res) => {
   try {
     const expenses = await Expense.find({ user: req.user.id });
@@ -16,7 +16,7 @@ router.get("/getallexpenses", fetchuser, async (req, res) => {
   }
 });
 
-//Add a new expense; Login Required
+//Route 2: Add a new expense; Login Required
 router.post(
   "/addexpense",
   fetchuser,
@@ -51,33 +51,73 @@ router.post(
   }
 );
 
-//Get update an existing expense; Login Required
-router.put(
-  "/updateexpense/:id",
-  fetchuser,
-  async (req, res) => {
-    const { title, type, amount, category, description, famMember } =
-        req.body;
-
-    const newExpense = {};
-    //only update the fields that are available in the body of the request
-    if(title){ newExpense.title = title}; 
-    if(type){ newExpense.type = type}; 
-    if(amount){ newExpense.amount = amount}; 
-    if(category){ newExpense.category = category}; 
-    if(description){ newExpense.description = description}; 
-    if(famMember){ newExpense.famMember = famMember}; 
+//Route 3: Update an existing expense; Login Required
+router.put("/updateexpense/:id", fetchuser, async (req, res) => {
+  try {
+    const { title, type, amount, category, description, famMember } = req.body;
 
     //check if the expense that the user is trying to access belongs to them
     let expense = await Expense.findById(req.params.id);
-    if(!expense){response.status(404).send("Not Found!")}
+    if (!expense) {
+      response.status(404).send("Not Found!");
+    }
 
-    if(expense.user.toString() !== req.user.id){
+    if (expense.user.toString() !== req.user.id) {
       return res.status(401).json("Not allowed");
     }
 
-    expense = await Expense.findByIdAndUpdate(req.params.id, {$set: newExpense}, {new:true});
+    const newExpense = {};
+    //only update the fields that are available in the body of the request
+    if (title) {
+      newExpense.title = title;
+    }
+    if (type) {
+      newExpense.type = type;
+    }
+    if (amount) {
+      newExpense.amount = amount;
+    }
+    if (category) {
+      newExpense.category = category;
+    }
+    if (description) {
+      newExpense.description = description;
+    }
+    if (famMember) {
+      newExpense.famMember = famMember;
+    }
+
+    expense = await Expense.findByIdAndUpdate(
+      req.params.id,
+      { $set: newExpense },
+      { new: true }
+    );
     res.send(expense);
-  });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+//Route 4: Delete an existing expense; Login Required
+router.delete("/deleteexpense/:id", fetchuser, async (req, res) => {
+  try {
+    //check if the expense that the user is trying to access belongs to them
+    let expense = await Expense.findById(req.params.id);
+    if (!expense) {
+      response.status(404).send("Not Found!");
+    }
+
+    if (expense.user.toString() !== req.user.id) {
+      return res.status(401).json("Not allowed"); 
+    }
+
+    expense = await Expense.findByIdAndDelete(req.params.id);
+    res.send({ Success: "Expense Deleted" });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
 module.exports = router;
