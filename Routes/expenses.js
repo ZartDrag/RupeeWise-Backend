5f44c1bb61bbad786b67f85f91b5cb050ddd5a27
ZartDrag <kartikmay25@gmail.com>
2023-02-06 17:09:00 +0530

    Update feature added

--- Routes/expenses.js
+++ Routes/expenses.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const fetchuser = require("../middleware/fetchUser");
 const Expense = require("../models/Expense");
 const { body, validationResult } = require("express-validator");
+const { response } = require("express");
 
 //Get all the expenses of a user; Login Required
 router.get("/getallexpenses", fetchuser, async (req, res) => {
@@ -49,5 +50,34 @@ router.post(
     }
   }
 );
+
+//Get update an existing expense; Login Required
+router.put(
+  "/updateexpense/:id",
+  fetchuser,
+  async (req, res) => {
+    const { title, type, amount, category, description, famMember } =
+        req.body;
+
+    const newExpense = {};
+    //only update the fields that are available in the body of the request
+    if(title){ newExpense.title = title}; 
+    if(type){ newExpense.type = type}; 
+    if(amount){ newExpense.amount = amount}; 
+    if(category){ newExpense.category = category}; 
+    if(description){ newExpense.description = description}; 
+    if(famMember){ newExpense.famMember = famMember}; 
+
+    //check if the expense that the user is trying to access belongs to them
+    let expense = await Expense.findById(req.params.id);
+    if(!expense){response.status(404).send("Not Found!")}
+
+    if(expense.user.toString() !== req.user.id){
+      return res.status(401).json("Not allowed");
+    }
+
+    expense = await Expense.findByIdAndUpdate(req.params.id, {$set: newExpense}, {new:true});
+    res.send(expense);
+  });
 
 module.exports = router;
